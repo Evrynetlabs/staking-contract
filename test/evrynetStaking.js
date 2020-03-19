82e437277e740474909942e077bc3cf59860930d
ducquangkstn <ducquang.kstn@gmail.com>
2020-03-19 17:33:32 +0700

    add test for resign withdraw unvote

--- test/evrynetStaking.js
+++ test/evrynetStaking.js
@@ -4,8 +4,8 @@ const BN = web3.utils.BN;
 
 const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
+const Helper = require("./helper.js");
 const { zeroAddress, oneEvrynet, assertEqual } = require("./helper.js");
-
 
 let admin;
 let epochPeriod = new BN(1024);
@@ -13,6 +13,8 @@ let startBlock = new BN(0);
 let maxValidatorsSize = new BN(40);
 let minValidatorStake = new BN(10).mul(oneEvrynet); // 10 evrynet
 let minVoterCap = new BN(oneEvrynet); // 1 evrynet
+let ownerUnlockPeriod = new BN(2);
+let voterUnlockPeriod = new BN(2);
 let stakingSC;
 let candidates;
 let owners;
@@ -51,6 +53,36 @@ contract("EvrynetStaking", function (accounts) {
                 assertEqual(candidateData[2], minValidatorStake, "unexected stakes");
             });
         });
+
+        it("getCurrentEpoch", async () => {
+            let blockNumber = await Helper.getCurrentBlock();
+            let expectedEpoch = getEpoch(new BN(blockNumber), startBlock, epochPeriod);
+            let actualEpoch = await stakingSC.getCurrentEpoch();
+            assertEqual(expectedEpoch, actualEpoch, "unexpected epoch");
+        });
+    });
+
+    describe("test admin set params", async () => {
+        let stakingSC;
+        before("using local variable so this will not affect global one", async () => {
+            stakingSC = await EvrynetStaking.new(candidates, owners, epochPeriod, startBlock, maxValidatorsSize, minValidatorStake, minVoterCap, admin);
+        });
+        it("updateMaxValidatorSize", async () => {
+            await stakingSC.updateMaxValidatorSize(new BN(80), { from: admin });
+            let maxValidatorsSize = await stakingSC.maxValidatorSize();
+            assertEqual(maxValidatorsSize, new BN(80), "unexpected maxValidatorsSize");
+        });
+        it("updateMinValidateStake", async () => {
+            await stakingSC.updateMinValidateStake(new BN(10).pow(new BN(5)), { from: admin });
+            let minValidatorStake = await stakingSC.minValidatorStake();
+            assertEqual(minValidatorStake, new BN(10).pow(new BN(5)), "unexpected max validator size");
+        });
+        it("updateMinVoteCap", async () => {
+            await stakingSC.updateMinVoteCap(new BN(3).mul(new BN(10).pow(new BN(5))), { from: admin });
+            let actualMinVoteCap = await stakingSC.minVoterCap();
+            assertEqual(actualMinVoteCap, new BN(3).mul(new BN(10).pow(new BN(5))), "unexpected minVoterCap");
+        });
+
     });
 
     describe("test transfer admin", async () => {
@@ -150,6 +182,7 @@ contract("EvrynetStaking", function (accounts) {
         });
     });
 
+
     describe("unvote", async () => {
         let votedCandidate;
         let votedOwners;
@@ -207,6 +240,102 @@ contract("EvrynetStaking", function (accounts) {
             //check new stake
             newVoterStake = await stakingSC.getVoterStake(votedCandidate, voter2);
             assertEqual(newVoterStake, minVoterCap, "unexpected new voter stake");
+
+            // check withdrawVoterStake
+            let unvoteEpoch = getEpoch(txResult.receipt.blockNumber, startBlock, epochPeriod);
+            let unlockpEpoch = unvoteEpoch.add(voterUnlockPeriod);
+            let withdrawVoterCap = await stakingSC.getWithdrawCap(unlockpEpoch, { from: voter2 });
+            assertEqual(withdrawVoterCap, new BN(1).mul(oneEvrynet), "unexpected withdrawVoterCap");
+
+            //unvote from other epoch and checks
+            let newBlock = getFirstBlock(unvoteEpoch + 1, startBlock, epochPeriod);
+            await Helper.increaseBlockNumberBySendingEther(admin, zeroAddress, new BN(newBlock).sub(new BN(txResult.receipt.blockNumber)).toNumber());
+            await stakingSC.unvote(votedCandidate, new BN(1).mul(oneEvrynet), { from: voter2 });
+
+            let withdrawEpochs = await stakingSC.getWithdrawEpochs({ from: voter2 });
+            assert(withdrawEpochs.length == 2, "unexpected withdrawEpochs.length");
+            assertEqual(withdrawEpochs[0], new BN(unlockpEpoch));
+            assertEqual(withdrawEpochs[1], new BN(unlockpEpoch).add(new BN(1)));
+
+            let data = await stakingSC.getWithdrawEpochsAndCaps({ from: voter2 });
+            assert(data[1].length == 2, "unexpected withdrawStakes.length");
+        });
+
+        it("withdraw after unvote", async () => {
+            let withdrawData = await stakingSC.getWithdrawEpochsAndCaps({ from: voter });
+            assert(withdrawData.epochs.length > 0, "empty withdrawData");
+            let withdrawValue = withdrawData.caps[0];
+            let withdrawEpoch = withdrawData.epochs[0];
+            // need to wait until withdrawEpoch, otherwise revert it
+            await expectRevert(stakingSC.withdraw(withdrawEpoch, { from: voter }), "can not withdraw for future epoch");
+
+            let unlockBlock = getFirstBlock(withdrawEpoch, startBlock, epochPeriod);
+            let currentBlock = await Helper.getCurrentBlock();
+            await Helper.increaseBlockNumberBySendingEther(admin, zeroAddress, new BN(unlockBlock).sub(new BN(currentBlock)).toNumber());
+            let initBalance = await Helper.getBalancePromise(voter);
+            let txGasPrice = new BN(10).pow(new BN(9));
+            let txResult = await stakingSC.withdraw(withdrawEpoch, { from: voter, gasPrice: txGasPrice });
+            let newBalance = await Helper.getBalancePromise(voter);
+            let actualReceiveAmount = new BN(newBalance).add(new BN(txGasPrice).mul(new BN(txResult.receipt.gasUsed))).sub(initBalance);
+            assertEqual(actualReceiveAmount, withdrawValue);
+            // no withdraw cap left to withdraw
+            await expectRevert(stakingSC.withdraw(withdrawEpoch, { from: voter }), "withdraw cap is 0");
         });
     });
-})
+
+
+    describe("test resign", async () => {
+        let stakingSC;
+        let candidates;
+        let owners;
+        before("recreate local staking contract", async () => {
+            candidates = [accounts[1], accounts[2], accounts[4]];
+            owners = [accounts[1], accounts[3], accounts[5]];
+            stakingSC = await EvrynetStaking.new(candidates, owners, epochPeriod, startBlock, maxValidatorsSize, minValidatorStake, minVoterCap, admin);
+        });
+
+        it("test revert if not owner", async () => {
+            await expectRevert(stakingSC.resign(candidates[0], { from: admin }), "not owner");
+        });
+
+        it("test revert if not candidate", async () => {
+            await expectRevert(stakingSC.resign(accounts[3], { from: admin }), "only active candidate");
+        });
+
+        it("test resign successfull", async () => {
+            //a person votes for this candidate before resign 
+            await stakingSC.vote(candidates[1], { from: accounts[6], value: minVoterCap });
+            let txResult = await stakingSC.resign(candidates[1], { from: owners[1] });
+            let resignEpoch = getEpoch(txResult.receipt.blockNumber, startBlock, epochPeriod);
+            expectEvent(txResult, "Resigned", {
+                _candidate: candidates[1],
+                _epoch: resignEpoch,
+            });
+            let isCandidate = await stakingSC.isCandidate(candidates[1]);
+            assert(!isCandidate, "candidate shoule be removed");
+            let currentOwnerStake = await stakingSC.getVoterStake(candidates[1], owners[1]);
+            assertEqual(currentOwnerStake, new BN(0), "current owner stake should be zero");
+            // voter of resign candidate still unvote to withdraw fund
+            await stakingSC.unvote(candidates[1], minVoterCap, { from: accounts[6] });
+            // check withdrawStake
+            let withdrawEpoch = resignEpoch.add(ownerUnlockPeriod);
+            let actualWithdrawCap = await stakingSC.getWithdrawCap(withdrawEpoch, { from: owners[1] });
+            assertEqual(actualWithdrawCap, minValidatorStake, "unexpeced withdraw cap");
+        });
+        it("test resign then register successful", async () => {
+            await stakingSC.resign(candidates[2], { from: owners[2] });
+            await stakingSC.register(candidates[2], accounts[8], { from: admin }); // new owner
+            let newOwner = await stakingSC.getCandidateOwner(candidates[2]);
+            assert(newOwner == accounts[8], "unexpected new owner");
+        });
+
+    });
+});
+
+function getEpoch(blockNumber, startBlock, epochPeriod) {
+    return new BN(blockNumber).sub(startBlock).div(epochPeriod);
+};
+
+function getFirstBlock(epoch, startBlock, epochPeriod) {
+    return new BN(epoch).mul(epochPeriod).add(startBlock);
+}
